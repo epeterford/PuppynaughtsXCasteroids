f840e5e2e789a6d05f23217c618bf0c823d9ef62
Morgan Laco <morgan@codeschool.com>
2016-09-17 18:04:54 -0400

    Implement two player controls

--- PuppynaughtsXCatsteroids/Assets/Scripts/Testplayer.cs
+++ PuppynaughtsXCatsteroids/Assets/Scripts/Testplayer.cs
@@ -1,116 +1,133 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Testplayer : MonoBehaviour {
 
+	public enum player {Player1 = 0, Player2 = 1};
+	public Dictionary<string, string> playerHorizonalControls = new Dictionary<string, string>();
+	public Dictionary<string, string> playerVerticalControls = new Dictionary<string, string>();
+	public player p;
 	float maxBoost;
 	float maxSpeed;
 	float speed;
 	float driftSpeed;
-    public float attachSpeed;
-    public float currentSpeed; 
+	public float attachSpeed;
+	public float currentSpeed; 
 
-    public bool isAttached;
+	public bool isAttached;
 	bool isBoosting;
 
 	public Rigidbody2D rb2D;
-    public Asteroid currentAsteroid; 
+	public Asteroid currentAsteroid; 
 
 
 	// Use this for initialization
 	void Start () 
-    {
-		maxSpeed = 3;
+	{
+		Debug.Log (p.ToString());
+		maxSpeed = 6;
 		speed = 10;
 		driftSpeed = 1f;
-        attachSpeed = 5f;
+		attachSpeed = 5f;
 		rb2D = GetComponent<Rigidbody2D> ();
 		rb2D.angularDrag = 3;
-	
-        isBoosting = false;
-        isAttached = false;
+
+		playerHorizonalControls.Add ("Player1", "P1 Horizontal");
+		playerHorizonalControls.Add ("Player2", "P2 Horizontal");
+		playerVerticalControls.Add ("Player1", "P1 Vertical");
+		playerVerticalControls.Add ("Player2", "P2 Vertical");
+
+		isBoosting = false;
+		isAttached = false;
 	}
-	
+
 	// Update is called once per frame
 	void Update () 
-    {
-        if(!isAttached)
-        {
-            Rotate();
-        }
-        if(Input.GetButtonDown("Detach") && isAttached)
-        {
-            Detach();
-        }
-        if(Input.GetButtonDown("Mine") && isAttached)
-        {
-            Mine();
-        }
+	{
+		if(!isAttached)
+		{
+			Rotate();
+		}
+		if(Input.GetButtonDown("Detach") && isAttached)
+		{
+			Detach();
+		}
+		if(Input.GetButtonDown("Mine") && isAttached)
+		{
+			Mine();
+		}
 	}
 
 	void FixedUpdate()
-    {
-        if(!isAttached)
-        {
-            Move();
-        }
-        else
-        {
-            rb2D.velocity = currentAsteroid.GetComponent<Rigidbody2D>().velocity; 
-        }
+	{
+		if(!isAttached)
+		{
+			Move();
+		}
+		else
+		{
+			rb2D.velocity = currentAsteroid.GetComponent<Rigidbody2D>().velocity; 
+		}
 
 	}
 
-    void Move()
-    {
-        //Friction
-        if (rb2D.velocity.magnitude > driftSpeed) {
-            Vector3 easeVelocity = rb2D.velocity;
-            easeVelocity.y *= .99f;
-            easeVelocity.z = 0.0f;
-            easeVelocity.x *= .99f;
-            rb2D.velocity = easeVelocity; 
-        }
+	void Move()
+	{
+		//Friction
+		if (rb2D.velocity.magnitude > driftSpeed) {
+			Vector3 easeVelocity = rb2D.velocity;
+			easeVelocity.y *= .99f;
+			easeVelocity.z = 0.0f;
+			easeVelocity.x *= .99f;
+			rb2D.velocity = easeVelocity; 
+		}
 
 
-        Vector3 keepRot = transform.eulerAngles;
+		Vector3 keepRot = transform.eulerAngles;
 
-        Vector2 dir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		//Vector2 dir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
-        transform.eulerAngles = keepRot;
+		transform.eulerAngles = keepRot;
 
-        currentSpeed = rb2D.velocity.magnitude;
-        if (currentSpeed > maxSpeed && Input.GetAxis("Vertical") !=0)
-        {
-            Debug.Log("Clamping");
-            rb2D.velocity = Vector2.ClampMagnitude (rb2D.velocity, maxSpeed);
-        }
-        else
-        {
-            rb2D.AddForce(transform.up*Input.GetAxis("Vertical")*speed);
+		currentSpeed = rb2D.velocity.magnitude;
 
-        }
-    }
+		// Vertical axis for this player
+		string whichVerticalAxis = playerVerticalControls[p.ToString()];
+		float verticalAxis = Input.GetAxis (whichVerticalAxis);
+		// Debug.Log (whichVerticalAxis.ToString () + " " + verticalAxis);
 
-    void Rotate()
-    {
-        transform.Rotate(0,0,Input.GetAxis("Horizontal")*Time.deltaTime*-180);
-    }
+		if (currentSpeed > maxSpeed && verticalAxis !=0)
+		{
+			Debug.Log("Clamping");
+			rb2D.velocity = Vector2.ClampMagnitude (rb2D.velocity, maxSpeed);
+		}
+		else
+		{
+			rb2D.AddForce(transform.up*verticalAxis*speed);
 
-    void Detach()
-    {
-        isAttached = false;
-        transform.parent = null;
-        currentAsteroid = null;
+		}
+	}
 
-    }
+	void Rotate()
+	{
+		transform.Rotate(0,0,Input.GetAxis("Horizontal")*Time.deltaTime*-180);
+	}
 
-    void Mine()
-    {
-        if(currentAsteroid)
-        {
-            Asteroid asteroid = currentAsteroid;
-            asteroid.StartMining();
-        }
-    }
+	void Detach()
+	{
+		isAttached = false;
+		transform.parent = null;
+		currentAsteroid = null;
+
+	}
+
+	void Mine()
+	{
+		if(currentAsteroid)
+		{
+			Asteroid asteroid = currentAsteroid;
+			asteroid.StartMining();
+		}
+	}
 }
