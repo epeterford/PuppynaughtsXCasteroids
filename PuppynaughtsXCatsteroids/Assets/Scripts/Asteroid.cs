5bacb6b97044d076533ac7f1a3c60968ce26c85b
Rennar <jwilliamson@knights.ucf.edu>
2016-09-17 15:23:58 -0400

    Asteroid Spawning

--- PuppynaughtsXCatsteroids/Assets/Scripts/Asteroid.cs
+++ PuppynaughtsXCatsteroids/Assets/Scripts/Asteroid.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Asteroid : MonoBehaviour {
 
 	public float maxSpeed;
+	public float startSpeed;
 
 	float scale;
 
@@ -16,23 +17,32 @@ public class Asteroid : MonoBehaviour {
 		scale = Random.Range (.1f, 2);
 		transform.localScale = new Vector3(scale,scale,scale);
 
-		points = 10*scale;
-
 		rb2D = GetComponent<Rigidbody2D>();
+
+		rb2D.mass = scale;
+
+		points = 10*scale;
 
 		rb2D.angularDrag = 2;
 
 		maxSpeed = 2;
 
-		rb2D.velocity = new Vector2(Random.Range(0,maxSpeed),Random.Range(0,maxSpeed));
+		startSpeed = Random.Range (.2f, maxSpeed);
 
-		if(rb2D.velocity.magnitude > maxSpeed){
-			rb2D.velocity = Vector2.ClampMagnitude(rb2D.velocity, maxSpeed);
-		}
+		rb2D.AddForce (maxSpeed * 20 * transform.up);
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+	void FixedUpdate(){
+		if(rb2D.velocity.magnitude > maxSpeed){
+			rb2D.velocity = Vector2.ClampMagnitude(rb2D.velocity, maxSpeed);
+		}
+	}
+		
 }
