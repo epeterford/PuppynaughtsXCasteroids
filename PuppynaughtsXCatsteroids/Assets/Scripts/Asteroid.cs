1176cbe22e8e769cc1244c5bf3d63670cee20c79
Rennar <jwilliamson@knights.ucf.edu>
2016-09-17 13:35:47 -0400

    Asteroids and stuff

--- PuppynaughtsXCatsteroids/Assets/Scripts/Asteroid.cs
+++ PuppynaughtsXCatsteroids/Assets/Scripts/Asteroid.cs
@@ -3,9 +3,32 @@ using System.Collections;
 
 public class Asteroid : MonoBehaviour {
 
+	public float maxSpeed;
+
+	float scale;
+
+	public float points;
+
+	Rigidbody2D rb2D;
+
 	// Use this for initialization
 	void Start () {
-	
+		scale = Random.Range (.1f, 2);
+		transform.localScale = new Vector3(scale,scale,scale);
+
+		points = 10*scale;
+
+		rb2D = GetComponent<Rigidbody2D>();
+
+		rb2D.angularDrag = 2;
+
+		maxSpeed = 2;
+
+		rb2D.velocity = new Vector2(Random.Range(0,maxSpeed),Random.Range(0,maxSpeed));
+
+		if(rb2D.velocity.magnitude > maxSpeed){
+			rb2D.velocity = Vector2.ClampMagnitude(rb2D.velocity, maxSpeed);
+		}
 	}
 	
 	// Update is called once per frame
--- PuppynaughtsXCatsteroids/Assets/Scripts/ScreenWrap.cs
+++ PuppynaughtsXCatsteroids/Assets/Scripts/ScreenWrap.cs
@@ -9,13 +9,10 @@ public class ScreenWrap : MonoBehaviour {
 	private Vector3 viewportsPosition;
 	public bool isWrappingX;
 	public bool isWrappingY;
+
 	public bool startOffScreen;
-	bool left;
-	bool up;
 
 	public Rigidbody2D rb2D;
-
-	public bool useUnstick;
 
 	float lastX;
 	float lastY;
@@ -23,7 +20,13 @@ public class ScreenWrap : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		cam = Camera.main;
-		renderer = GetComponent<MeshRenderer> ();
+
+		if (!GetComponent<MeshRenderer> ()) {
+			renderer = GetComponentInChildren<MeshRenderer> ();
+		} else {
+			renderer = GetComponent<MeshRenderer> ();
+		}
+
 		viewportsPosition = cam.WorldToViewportPoint (transform.position);
 		isWrappingX = false;
 		isWrappingY = false;
@@ -43,14 +46,11 @@ public class ScreenWrap : MonoBehaviour {
 
 	//Screen Wrap functions
 	bool CheckRenderers(){
-		/*foreach (MeshRenderer mr in renderers) {
-			if (mr.isVisible) {
-				return true;
-			}
-		}*/
+		
 		if (renderer.isVisible) {
 			return true;
 		}
+
 		return false;
 	}
 
@@ -60,22 +60,6 @@ public class ScreenWrap : MonoBehaviour {
 		if (isVisible) {
 			isWrappingX = false;
 			isWrappingY = false;
-
-			lastX = transform.position.x;
-			if (lastX < 0) {
-				left = true;
-			} else {
-				left = false;
-			}
-
-			lastY = transform.position.y;
-			if (lastY > 0) {
-				up = true;
-			} else {
-				up = false;
-			}
-
-			return;
 		}
 
 		if (isWrappingX && isWrappingY) {
@@ -100,17 +84,11 @@ public class ScreenWrap : MonoBehaviour {
 		if (!isWrappingY && (viewportsPosition.y > 1 || viewportsPosition.y < 0)) {
 
 			newPosition.y = -newPosition.y;
-			if (rb2D.velocity.y < -15) {
-				rb2D.velocity = new Vector2 (rb2D.velocity.x, -15);
-			}
 			isWrappingY = true;
 		}
 
 		transform.position = newPosition;
 
-		if(useUnstick){
-			StartCoroutine ("UnStick");
-		}
 
 	}
 
@@ -118,41 +96,5 @@ public class ScreenWrap : MonoBehaviour {
 		renderer = GetComponent<MeshRenderer> ();
 	}
 
-	IEnumerator UnStick(){
-		bool stuck = true;
-		float timer = 0;
-		while (stuck) {
-			if (timer >= .05f) {
-				Debug.Log ("Unsticking");
-				if (left) {
-					transform.position = new Vector3 (lastX + .1f, transform.position.y, 0f);
-					stuck = false;
-				} else {
-					transform.position = new Vector3 (lastX - .1f, transform.position.y, 0f);
-					stuck = false;
-				}
-				if (up) {
-					transform.position = new Vector3 (transform.position.x, lastY-.1f, 0f);
-					stuck = false;
-				} else {
-					transform.position = new Vector3 (transform.position.x, lastY*-1, 0f);
-					stuck = false;
-				}
-			}
-			/*foreach (MeshRenderer mr in renderers) {
-				if (mr.isVisible) {
-					stuck = false;
-					break;
-				}
-			}*/
-			if (renderer.isVisible) {
-				stuck = false;
-			}
 
-
-			timer += Time.deltaTime;
-			yield return new WaitForSeconds (.1f);
-		}
-		yield break;
-	}
 }
